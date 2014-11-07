b6ab21314a9db7464bfcf63c6c6d5f23762c48ea
Joel Segerlind <joel@burtcorp.com>
2014-11-07 15:59:21 +0100

    Don't use projectile everywhere

--- packages.el
+++ packages.el
@@ -43,7 +43,6 @@
 (use-package projectile
   :init (projectile-global-mode)
   :config (progn
-	    (setq projectile-require-project-root nil)
             (add-hook 'projectile-mode-hook (lambda () (bind-key "C-c p a" 'ack-wrap projectile-mode-map)))))
 
 
