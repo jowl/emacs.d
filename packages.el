d21fe9e43eb57e92c989f7e32f07120dd4338465
Joel Segerlind <joel@burtcorp.com>
2015-01-01 18:30:50 +0100

    Add some haskell-mode hooks

--- packages.el
+++ packages.el
@@ -146,3 +146,7 @@
 (use-package ibuffer-vc
   :init (ibuffer-vc-set-filter-groups-by-vc-root)
   :bind ("C-x C-b" . ibuffer))
+
+(use-package haskell-mode
+  :init (progn (add-hook 'haskell-mode-hook 'haskell-indent-mode)
+               (add-hook 'haskell-mode-hook 'interactive-haskell-mode)))
