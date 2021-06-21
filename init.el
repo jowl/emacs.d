69922705e495a5f19c9d46b5d484bc58d63c3835
Joel Segerlind <joel.segerlind@gmail.com>
2021-06-21 07:54:21 +0200

    Add some packages

--- init.el
+++ init.el
@@ -258,7 +258,7 @@ there's a region, all lines that region covers will be duplicated."
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    '("c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "62f68a0b49cf383478041c688cc1b82f084f76b84a2ab2819a4ed9ceb59aefd8" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default))
- '(package-selected-packages '(f use-package)))
+ '(package-selected-packages '(kotlin-mode markdown-mode jq-mode f use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
